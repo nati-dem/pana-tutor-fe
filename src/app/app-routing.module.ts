9675ae925f431d50b6df8e208f7f22c4d19b0f81
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-05-02 16:18:43 -0500

    add ng-bootstrap and sample module with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'courses', loadChildren: () => import('./course/course.module').then(m => m.CourseModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
