30a7fa2fd6ac379b93f729664f0a887ff17595c9
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-05-03 11:53:04 -0500

    update auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '',
-    pathMatch: 'full'
-  },
-  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: '',redirectTo: '',pathMatch: 'full'},
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   { path: 'categories', loadChildren: () => import('./category/category.module').then(m => m.CategoryModule) }
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
