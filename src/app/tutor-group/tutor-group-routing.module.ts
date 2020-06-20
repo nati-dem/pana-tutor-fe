fdb15f8934b8e565be661a7dcbc3669a17e4c564
Natnael Demisse <natnael.demisse@siriusxm.com>
2020-06-20 12:38:13 -0500

    update tutor group module

--- src/app/tutor-group/tutor-group-routing.module.ts
+++ src/app/tutor-group/tutor-group-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { TutorGroupAdminComponent } from "./tutor-group-admin/tutor-group-admin.component";
-import { TutorBookingComponent } from "./tutor-booking/tutor-booking.component";
 import { from } from "rxjs";
 
 const routes: Routes = [
   //{ path: "", component: TutorProfileListComponent },
   //{ path: "admin/:courseId", component: TutorGroupAdminComponent },
-  { path: "booking", component: TutorBookingComponent },
 ];
 
 @NgModule({
--- src/app/tutor-group/tutor-group.module.ts
+++ src/app/tutor-group/tutor-group.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TutorGroupRoutingModule } from './tutor-group-routing.module';
 import { TutorGroupAdminComponent } from './tutor-group-admin/tutor-group-admin.component';
-import { TutorBookingComponent } from './tutor-booking/tutor-booking.component';
 
 @NgModule({
-  declarations: [TutorGroupAdminComponent, TutorBookingComponent],
+  declarations: [TutorGroupAdminComponent],
   imports: [
     CommonModule,
     TutorGroupRoutingModule
